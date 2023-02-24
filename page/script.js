510545459e40429d606bb4a6952f33971efc763d
majg8a <majg8a@gmail.com>
2023-02-23 21:20:12 -0400

    scope

--- page/script.js
+++ page/script.js
@@ -1,12 +1,9 @@
 const video = document.createElement("video");
-
-function main() {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then((signal) => {
-      video.srcObject(signal);
-      document.body.appendChild(video);
-      video.play();
-    })
-    .catch(alert);
-}
+navigator.mediaDevices
+  .getUserMedia({ video: true })
+  .then((signal) => {
+    video.srcObject(signal);
+    document.body.appendChild(video);
+    video.play();
+  })
+  .catch(alert);
