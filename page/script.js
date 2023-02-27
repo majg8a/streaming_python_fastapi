c01338d6d8007ee9822a45a651d89d1c38b8f175
Marcelo Gil <majgil@grupoctg.local>
2023-02-27 17:41:05 -0400

    feat: blob to stream

--- page/script.js
+++ page/script.js
@@ -1,30 +1,43 @@
+const blobToImage = async (blob = new Blob([])) => {
+  const img = new Image();
+  img.src = URL.createObjectURL(blob);
+  await new Promise((res) => (img.onload = res));
+  return img;
+};
+
+const videoToFrameBlob = async (video) => {
+  const canvas = document.createElement("canvas");
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  const context = canvas.getContext("2d");
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  return await new Promise((res) => canvas.toBlob(res));
+};
+
 (async () => {
   const video = document.createElement("video");
-
   const mediastream = await navigator.mediaDevices
-    .getUserMedia({ video: true })
+    .getUserMedia({ video: { width: 720, height: 360 } })
     .catch(alert);
+  video.srcObject = mediastream;
+  document.body.append(video);
+  const canvas = document.createElement("canvas");
 
-  // setInterval(() => {
-  //   const imageCapture = new ImageCapture(stream.getVideoTracks()[0]);
-  // }, 1000);
-  // ws.onopen = () => setInterval(captureImage, 1000 / FPS); // ws is new WebSocket(WS_URL)
-
+  const context = canvas.getContext("2d");
+  document.body.append(canvas);
   const socket = new WebSocket("ws://localhost:5000/ws");
 
-  // Connection opened
-  socket.addEventListener("open", (event) => {
-    const recorder = new MediaRecorder(mediastream);
-
-    recorder.ondataavailable = ({ data }) => {
-      data ? socket.send(data) : null;
-    };
-
-    recorder.start(1000);
+  socket.addEventListener("open", async (event) => {
+    await video.play();
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const frameBlob = await videoToFrameBlob(video);
+    socket.send(frameBlob);
   });
 
-  // Listen for messages
-  socket.addEventListener("message", (event) => {
-    console.log("Message from server ", event.data);
+  socket.addEventListener("message", async ({ data }) => {
+    const img = await blobToImage(data);
+    context.drawImage(img, 0, 0, img.width, img.height);
+    setTimeout(async () => socket.send(await videoToFrameBlob(video)), 0);
   });
 })();
