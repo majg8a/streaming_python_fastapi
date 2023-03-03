7f6eb3414d836fce66a3d784e69e4337ef3c5bc3
majg8a <majg8a@gmail.com>
2023-03-02 22:05:59 -0400

    feat: local ui

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Header, WebSocket
-from fastapi.responses import Response, StreamingResponse
-from os import getcwd, path
+# from fastapi import FastAPI, Header, WebSocket
+from fastapi import FastAPI, WebSocket
+# from fastapi.responses import Response, StreamingResponse, FileResponse
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
 
 # PORTION_SIZE = 1024 * 1024
 
@@ -21,6 +25,9 @@ app = FastAPI()
 #             yield from file
 
 #     return StreamingResponse(iterfile(), media_type="video/mp4")
+# @app.get("/", response_class=HTMLResponse)
+# async def pageUI(request):
+#     return templates.TemplateResponse("index.html", context={"request": request})
 
 
 @app.websocket("/ws")
@@ -31,16 +38,26 @@ async def websocket_endpoint(websocket: WebSocket):
         await websocket.send_bytes(data)
 
 
+@app.get("/", response_class=RedirectResponse)
+def redirect(request):
+    print(f"dfasdfasdfasdf {request.url._url}")
+    return f"{request.url._url}/page/index.html"
+
+
+templates = Jinja2Templates(directory="/")
+app.mount("/page", StaticFiles(directory="./page"), name="page")
+
+
 # https://fastapi.tiangolo.com/advanced/websockets/?h=web
 
-    # with open(filePath, "rb") as file:
-    #     file.seek(start)
-    #     data = file.read(end - start)
-    #     sizeVideo = str(path.getsize(filePath))
+# with open(filePath, "rb") as file:
+#     file.seek(start)
+#     data = file.read(end - start)
+#     sizeVideo = str(path.getsize(filePath))
 
-    #     headers = {
-    #         'Content-Range': f'bytes {str(start)}-{str(end)}/{sizeVideo}',
-    #         'Accept-Ranges': 'bytes'
-    #     }
+#     headers = {
+#         'Content-Range': f'bytes {str(start)}-{str(end)}/{sizeVideo}',
+#         'Accept-Ranges': 'bytes'
+#     }
 
-    #     return Response(content=data, status_code=206, headers=headers, media_type="video/mp4")
+#     return Response(content=data, status_code=206, headers=headers, media_type="video/mp4")
